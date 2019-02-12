b2244ba8ef19e2788b7c96b13516e85ae28f349a
ismaan1998 <indrajeetsinghmaan@gmail.com>
2019-02-12 15:13:50 +0530

    Update file_search.py

--- file_search.py
+++ file_search.py
@@ -1,6 +1,16 @@
 import os
+import platform
 
+
+os_info = platform.platform()
+path = '/'
 files_total = []
+
+def set_root(s):
+    global path
+    path = s
+
+
 #this function is working for function file_search()
 def file_search2(name, name_words):
     check = True
@@ -8,7 +18,6 @@ def file_search2(name, name_words):
         if word not in name:
             check = False
             break
-
     return check
 
 #search a file and open it.
@@ -20,17 +29,17 @@ def searchFile(filename):
     file_found = False
     words = filename.split()
 
-    for root, dirs, files in os.walk('/', topdown=False):
+    for root, dirs, files in os.walk(path, topdown=False):
 
         for name in files:
             l_name = name.lower()
 
+            os_info = platform.platform()
+
             if file_search2(l_name, words):
                 file_found = True
-                files_total.append(root+name)
-
-
+                if('Windows' in os_info):
+                    files_total.append(root+'\\'+name)
+                else:
+                    files_total.append(root+'/'+name)
     return files_total
-
-    if(not file_found):
-        print("not found")
